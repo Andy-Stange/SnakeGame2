9b6eb70965d43084fa31cbbeb4bde3ca92a51352
Andy-Stange <64036959+Andy-Stange@users.noreply.github.com>
2020-05-06 00:12:17 -0500

    Added Player Movement

--- SnakeGame2/Assets/Scenes/Scripts/GameManager.cs
+++ SnakeGame2/Assets/Scenes/Scripts/GameManager.cs
@@ -1,45 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 namespace Snakes
 {
     public class GameManager : MonoBehaviour
     {
         public int maxHeight = 20;
         public int maxWidth = 20;
-
         public Color color1;
         public Color color2;
         public Color playerColor = Color.gray;
 
+        public Transform cameraHolder;
+
         GameObject playerObj;
+        Node playerNode;
 
         GameObject mapObject;
         SpriteRenderer mapRenderer;
 
         Node[,] grid;
+
+        bool up, left, right, down;
+
+        public float moveRate = 0.5f;
+        float timer;
+
+        Direction curDirection;
+        public enum Direction
+        {
+            up, down, left, right
+        }
+
+        #region Init
         private void Start()
         {
             CreateMap();
             PlacePlayer();
+            PlaceCamera();
         }
 
         void CreateMap()
         {
             mapObject = new GameObject("Map");
             mapRenderer = mapObject.AddComponent<SpriteRenderer>();
-
             grid = new Node[maxWidth, maxHeight];
-
             Texture2D txt = new Texture2D(maxWidth, maxHeight);
             for (int x = 0; x < maxWidth; x++)
             {
                 for (int y = 0; y < maxHeight; y++)
                 {
                     Vector3 tp = Vector3.zero;
-                    tp.x = x;
-                    tp.y = y;
+                    tp.x = x + 1;
+                    tp.y = y + 1;
 
                     Node n = new Node()
                     {
@@ -47,7 +60,6 @@ namespace Snakes
                         y = y,
                         worldPosition = tp
                     };
-
                     grid[x, y] = n;
                     #region Visual
                     if (x % 2 != 0)
@@ -59,7 +71,6 @@ namespace Snakes
                         else
                         {
                             txt.SetPixel(x, y, color2);
-
                         }
                     }
                     else
@@ -67,23 +78,20 @@ namespace Snakes
                         if (y % 2 != 0)
                         {
                             txt.SetPixel(x, y, color2);
-
                         }
                         else
                         {
                             txt.SetPixel(x, y, color1);
-
                         }
                     }
                     #endregion
-
                 }
             }
             txt.filterMode = FilterMode.Point;
 
             txt.Apply();
             Rect rect = new Rect(0, 0, maxWidth, maxHeight);
-            Sprite sprite = Sprite.Create(txt, rect, Vector2.one * .5f, 1, 0, SpriteMeshType.FullRect);
+            Sprite sprite = Sprite.Create(txt, rect, Vector2.zero, 1, 0, SpriteMeshType.FullRect);
             mapRenderer.sprite = sprite;
         }
 
@@ -95,8 +103,97 @@ namespace Snakes
             playerRender.sortingOrder = 1;
 
             playerObj.transform.position = GetNode(3, 3).worldPosition;
+            playerNode = GetNode(3, 3);
+            playerObj.transform.position = playerNode.worldPosition;
         }
 
+        void PlaceCamera()
+        {
+            Node n = GetNode(maxWidth / 2, maxHeight / 2);
+            Vector3 p = n.worldPosition;
+            p += Vector3.one * 5f;
+            cameraHolder.position = n.worldPosition;
+        }
+        #endregion
+
+        #region Updade
+        private void Update()
+        {
+            GetInput();
+            SetPlayerDirection();
+
+            timer += Time.deltaTime;
+            if (timer > moveRate)
+            {
+                timer = 0;
+                MovePlayer();
+            }
+
+            MovePlayer();
+        }
+        void GetInput()
+        {
+            up = Input.GetButtonDown("Up");
+            down = Input.GetButtonDown("Down");
+            left = Input.GetButtonDown("Left");
+            right = Input.GetButtonDown("Right");
+        }
+        void SetPlayerDirection()
+        {
+            if (up)
+            {
+                curDirection = Direction.up;
+            }
+            else if (down)
+            {
+                curDirection = Direction.down;
+            }
+            else if (left)
+            {
+                curDirection = Direction.left;
+            }
+            else if (right)
+            {
+                curDirection = Direction.right;
+            }
+        }
+
+        void MovePlayer()
+        {
+            int x = 0;
+            int y = 0;
+
+            switch (curDirection)
+            {
+                case Direction.up:
+                    y = 1;
+                    break;
+                case Direction.down:
+                    y = -1;
+                    break;
+                case Direction.left:
+                    x = -1;
+                    break;
+                case Direction.right:
+                    x = 1;
+                    break;
+            }
+            Node targetNode = GetNode(playerNode.x + x, playerNode.y + y);
+            if (targetNode == null)
+            {
+                //game Over
+            }
+            else
+            {
+                playerObj.transform.position = targetNode.worldPosition;
+                playerNode = targetNode;
+            }
+        }
+
+
+        #endregion
+
+        #region Utilities
         Node GetNode(int x, int y)
         {
             if (x < 0 || x > maxWidth - 1 || y < 0 || y > maxHeight - 1)
@@ -106,7 +203,6 @@ namespace Snakes
 
             return grid[x, y];
         }
-
         Sprite CreateSprite(Color targetColor)
         {
             Texture2D txt = new Texture2D(1, 1);
@@ -116,5 +212,6 @@ namespace Snakes
             Rect rect = new Rect(0, 0, 1, 1);
             return Sprite.Create(txt, rect, Vector2.one * 1f, 1, 0, SpriteMeshType.FullRect);
         }
+        #endregion
     }
 }
