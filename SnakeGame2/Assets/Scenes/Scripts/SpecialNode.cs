6879f484cc859ef768ddbdb487937ffcbb7a9330
Andy-Stange <64036959+Andy-Stange@users.noreply.github.com>
2020-05-05 23:56:42 -0500

    Created Grid and Player Node

--- SnakeGame2/Assets/Scenes/Scripts/SpecialNode.cs
+++ SnakeGame2/Assets/Scenes/Scripts/SpecialNode.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManage : MonoBehaviour
+public class SpecialNode : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
