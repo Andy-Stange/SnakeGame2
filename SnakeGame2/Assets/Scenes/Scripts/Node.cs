2e73ba01518410045ca5ff0ba0525c3c65d122a7
Andy-Stange <64036959+Andy-Stange@users.noreply.github.com>
2020-05-07 14:42:17 -0500

    Added a couple extra features

--- SnakeGame2/Assets/Scenes/Scripts/Node.cs
+++ SnakeGame2/Assets/Scenes/Scripts/Node.cs
@@ -9,5 +9,16 @@ namespace Snakes
         public int x;
         public int y;
         public Vector3 worldPosition;
+
+        public Node(int num1, int num2, Vector3 vec)
+        {
+            x = num1;
+            y = num2;
+            worldPosition = vec;
+        }
+        public Node()
+        {
+
+        }
     }
 }
